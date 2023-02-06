22258f2a71e1ae02305939cf8efcece2ef37a2cd
donassere <donatien.gauthier@ynov.com>
2023-02-06 12:00:09 +0100

    entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,7 +16,7 @@ import { UsersModule } from './users/users.module';
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
+        type: 'mysql',
         host: configService.get('DB_HOST'),
         port: +configService.get<number>('DB_PORT'),
         username: configService.get('DB_USERNAME'),
