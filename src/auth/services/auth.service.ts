7e78c3ef03d19ec244214627dc6746095f073333
Jclouxdev <joseph.clx@gmail.com>
2023-02-13 09:32:33 +0100

    :art: | Linter

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -1,38 +1,36 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from '../../users/user.service';
 import { JwtService } from '@nestjs/jwt';
-import { HttpException, HttpStatus} from '@nestjs/common';
+import { HttpException, HttpStatus } from '@nestjs/common';
 import { UserEntity } from '../../users/user.entity';
 import * as bcrypt from 'bcrypt';
-
 
 @Injectable()
 export class AuthService {
   constructor(
     private usersService: UsersService,
-    private jwtService: JwtService
+    private jwtService: JwtService,
   ) {}
 
   async validateUser(email: string, password: string): Promise<UserEntity> {
     const user = await this.usersService.FindOneEmail(email);
-    const passwordValid = await bcrypt.compare(password, user.password)
+    const passwordValid = await bcrypt.compare(password, user.password);
     if (user && !passwordValid) {
-        throw new HttpException('Invalid credentials', HttpStatus.UNAUTHORIZED);
+      throw new HttpException('Invalid credentials', HttpStatus.UNAUTHORIZED);
     }
-    if (!user){
-      throw new HttpException('invalid credentials', HttpStatus.UNAUTHORIZED )
-    };
+    if (!user) {
+      throw new HttpException('invalid credentials', HttpStatus.UNAUTHORIZED);
+    }
     return user;
   }
 
-
-  async login(email:string, password:string) {
-    const validatedUser = await this.validateUser(email, password)
-    const payload = { 
+  async login(email: string, password: string) {
+    const validatedUser = await this.validateUser(email, password);
+    const payload = {
       email: validatedUser.email,
       id: validatedUser.id,
     };
-    console.log(validatedUser)
+    console.log(validatedUser);
     return {
       access_token: this.jwtService.sign(payload),
     };
